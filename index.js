cb644343e04e3bd9501d0a4accc6901f460bfc07
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2025-01-16 05:59:56 +0600

    server initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Donor Network is running");
+});
+
+app.listen(port, () => {
+  console.log(`Donor Network is running on port: ${port}`);
+});
