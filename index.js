1b014a395812c5f40138fd368440166325d10621
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2025-01-17 07:33:34 +0600

    post api added for users

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("donorNetworkDb").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
