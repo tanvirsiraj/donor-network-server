6918ebb165af532d932dd831c26b8993a72a8bdd
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2025-04-14 00:16:14 +0600

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 // middleware
 app.use(cors());
